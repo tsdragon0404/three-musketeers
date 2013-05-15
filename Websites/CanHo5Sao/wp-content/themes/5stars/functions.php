18dca9d00a762d31747f6e2e344a6948247236c6
tsdragon0404@gmail.com <tsdragon0404@gmail.com@c35c649d-0493-fe16-f86e-a3b88fa47fd3>
2013-05-15 16:15:29 +0000

    edit

--- Websites/CanHo5Sao/wp-content/themes/5stars/functions.php
+++ Websites/CanHo5Sao/wp-content/themes/5stars/functions.php
@@ -1,3 +1,5 @@
-require( get_template_directory() . '../5stars/function_D.php' );
-require( get_template_directory() . '../5stars/function_L.php' );
-require( get_template_directory() . '../5stars/function_S.php' );
+<?php
+
+require( str_replace("twentyeleven","",get_template_directory()) . '5stars/functions_D.php' );
+require( str_replace("twentyeleven","",get_template_directory()) . '5stars/functions_L.php' );
+require( str_replace("twentyeleven","",get_template_directory()) . '5stars/functions_S.php' );
