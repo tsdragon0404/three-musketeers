63dbfad43ce0f6c3872cdc106a10c2ccc3304921
grayboneonline@gmail.com <grayboneonline@gmail.com@c35c649d-0493-fe16-f86e-a3b88fa47fd3>
2013-09-17 20:41:31 +0000

    Create AppCenter version 2.0. Totally re-design structure

---
+++ WP8_AppCenter/AppCenter_2.0/AppCenter/ViewModel/AppViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using AppCenter.Data;
+
+namespace AppCenter.ViewModel
+{
+    public class AppViewModel : INotifyPropertyChanged
+    {
+        private AppCenterDataContext _appCenterDatacontext = new AppCenterDataContext();
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ WP8_AppCenter/AppCenter_2.0/LS.Core/BaseModel.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace LS.Core
+{
+    public class BaseModel : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        private int _id;
+
+        [Column(IsPrimaryKey = true, IsDbGenerated = true, DbType = "INT NOT NULL Identity", CanBeNull = false, AutoSync = AutoSync.OnInsert)]
+        public int ID
+        {
+            get { return _id; }
+            set
+            {
+                if (_id == value) return;
+
+                NotifyPropertyChanging("ID");
+                _id = value;
+                NotifyPropertyChanged("ID");
+            }
+        }
+
+        [Column(IsVersion = true)]
+        private Binary _version;
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanging Members
+
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        protected void NotifyPropertyChanging(string propertyName)
+        {
+            if (PropertyChanging != null)
+            {
+                PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
