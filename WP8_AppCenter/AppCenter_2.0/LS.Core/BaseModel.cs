421a67b030f28005f75d0bb163464ea407e4157e
grayboneonline@gmail.com <grayboneonline@gmail.com@c35c649d-0493-fe16-f86e-a3b88fa47fd3>
2013-09-18 14:15:49 +0000

    Create MainPage - Nokia apps

--- WP8_AppCenter/AppCenter_2.0/LS.Core/BaseModel.cs
+++ WP8_AppCenter/AppCenter_2.0/LS.Core/BaseModel.cs
@@ -1,30 +1,9 @@
 ﻿using System.ComponentModel;
-using System.Data.Linq;
-using System.Data.Linq.Mapping;
 
 namespace LS.Core
 {
     public class BaseModel : INotifyPropertyChanged, INotifyPropertyChanging
     {
-        private int _id;
-
-        [Column(IsPrimaryKey = true, IsDbGenerated = true, DbType = "INT NOT NULL Identity", CanBeNull = false, AutoSync = AutoSync.OnInsert)]
-        public int ID
-        {
-            get { return _id; }
-            set
-            {
-                if (_id == value) return;
-
-                NotifyPropertyChanging("ID");
-                _id = value;
-                NotifyPropertyChanged("ID");
-            }
-        }
-
-        [Column(IsVersion = true)]
-        private Binary _version;
-
         #region INotifyPropertyChanged Members
 
         public event PropertyChangedEventHandler PropertyChanged;
