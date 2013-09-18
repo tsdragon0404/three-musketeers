bfea2cd9ec4f5a0b7b975a2f87d054cf09ae24d1
grayboneonline@gmail.com <grayboneonline@gmail.com@c35c649d-0493-fe16-f86e-a3b88fa47fd3>
2013-09-18 18:44:18 +0000

    Show all category + create base command

---
+++ trunk/WP8_AppCenter/AppCenter_2.0/LS.Core/BaseCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace LS.Core
+{
+    public class BaseCommand : ICommand
+    {
+        protected Action Action = null;
+        protected Action<object> ParameterizedAction = null;
+
+        #region Constructors
+
+        public BaseCommand(Action action, bool canExecute = true)
+        {
+            Action = action;
+            _canExecute = canExecute;
+        }
+
+        public BaseCommand(Action<object> parameterizedAction, bool canExecute = true)
+        {
+            ParameterizedAction = parameterizedAction;
+            _canExecute = canExecute;
+        } 
+
+        #endregion
+
+        private bool _canExecute;
+
+        public bool CanExecute
+        {
+            get { return _canExecute; }
+            set
+            {
+                if (_canExecute != value)
+                {
+                    _canExecute = value;
+                    var canExecuteChanged = CanExecuteChanged;
+                    if (canExecuteChanged != null)
+                        canExecuteChanged(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return _canExecute;
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            if(Action != null)
+                Action();
+            if (ParameterizedAction != null)
+                ParameterizedAction(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
