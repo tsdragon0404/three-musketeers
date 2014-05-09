6b95c7eeb190593aed37aeb8a6c62ce52c72fc3a
tsdragon0404@gmail.com <tsdragon0404@gmail.com@c35c649d-0493-fe16-f86e-a3b88fa47fd3>
2014-05-09 17:03:18 +0000

    update

--- SMS/SMS.Business/IInvoiceDetailManagement.cs
+++ SMS/SMS.Business/IInvoiceDetailManagement.cs
@@ -6,5 +6,6 @@ namespace SMS.Business
     public interface IInvoiceDetailManagement : IBaseManagement<InvoiceDetailDto, long>
     {
         InvoiceDetailDto AddProductToInvoiceTable(long invoiceTableID, long productID, int quantity);
+        InvoiceDetailDto UpdateProductToInvoiceTable(long invoiceDetailID, string columnName, string value);
     }
 }
