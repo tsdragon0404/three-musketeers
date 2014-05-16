abcede78912360c402d2e1b58e355085de8009b7
tsdragon0404@gmail.com <tsdragon0404@gmail.com@c35c649d-0493-fe16-f86e-a3b88fa47fd3>
2014-05-16 07:12:58 +0000

    add Currency and edit Branch

---
+++ SMS/SMS.Data.Mapping/BranchMap.cs
@@ -0,0 +1,17 @@
+﻿using SMS.Data.Entities;
+
+namespace SMS.Data.Mapping
+{
+    public class BranchMap : BaseMap<Branch>
+    {
+        public BranchMap()
+        {
+            Table("Branch");
+            Map(x => x.VNName);
+            Map(x => x.ENName);
+            References(x => x.Currency).Column("CurrencyID");
+            Map(x => x.UseServiceFee);
+            Map(x => x.ServiceFee);
+        }
+    }
+}
