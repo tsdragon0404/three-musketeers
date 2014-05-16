c42802c8eb19ac5cca489efb94b68f189aabed3c
tsdragon0404@gmail.com <tsdragon0404@gmail.com@c35c649d-0493-fe16-f86e-a3b88fa47fd3>
2014-05-16 07:00:29 +0000

    update branchTax map

---
+++ SMS/SMS.Data.Entities/Tax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Core.Data;
+
+namespace SMS.Data.Entities
+{
+    public class Tax : Entity, IAuditableEntity, ISortableEntity, IEnableEntity
+    {
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+        public virtual decimal Value { get; set; }
+
+        #region Implementation of IEnableEntity
+
+        public virtual bool Enable { get; set; }
+
+        #endregion
+
+        #region Implementation of ISortableEntity
+
+        public virtual int SEQ { get; set; }
+
+        #endregion
+
+        #region Implementation of IAuditableEntity
+
+        public virtual DateTime? CreatedDate { get; set; }
+
+        public virtual string CreatedUser { get; set; }
+
+        public virtual DateTime? ModifiedDate { get; set; }
+
+        public virtual string ModifiedUser { get; set; }
+
+        #endregion
+    }
+}
