8d3afc16dd82a344bfd2472b796c4f6ff63cdc76
grayboneonline@gmail.com <grayboneonline@gmail.com@c35c649d-0493-fe16-f86e-a3b88fa47fd3>
2014-05-29 11:06:24 +0000

    structure for ErrorMessage

---
+++ trunk/SMS/SMS.Data.Dtos/ErrorMessageDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SMS.Data.Dtos
+{
+    public class ErrorMessageDto 
+    {
+        public virtual long ID { get; set; }
+
+        public virtual string VNMessage { get; set; }
+
+        public virtual string ENMessage { get; set; }
+    }
+}
--- trunk/SMS/SMS.Data.Mapping/PageMap.cs
+++ trunk/SMS/SMS.Data.Mapping/PageMap.cs
@@ -1,12 +1,14 @@
-﻿using SMS.Data.Entities;
+﻿using FluentNHibernate.Mapping;
+using SMS.Data.Entities;
 
 namespace SMS.Data.Mapping
 {
-    public class PageMap : BaseMap<Page>
+    public class PageMap : ClassMap<Page>
     {
         public PageMap()
         {
             Table("Page");
+            Id(x => x.ID).Column("PageID");
             Map(x => x.VNTitle);
             Map(x => x.ENTitle);
             Map(x => x.VNDescription);
