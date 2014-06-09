f1037b81ae8095996404d0ff77b5a8c1dbd0e4aa
grayboneonline@gmail.com <grayboneonline@gmail.com@c35c649d-0493-fe16-f86e-a3b88fa47fd3>
2014-06-09 16:00:45 +0000

    validate and format cashier input

--- SMS/SMS.MvcApplication/Scripts/custom-validate.js
+++ SMS/SMS.MvcApplication/Scripts/custom-validate.js
@@ -3,12 +3,19 @@
         $(element).attr('data-error-msg', error.html());
         if ($(element).parent().hasClass('ui-spinner'))
             $(element).parent().addClass('ui-state-error');
+        if($(element).prop('tagName') == 'INPUT')
+            $(element).addClass('ui-state-error');
     },
     success: function(label, element) {
-    if ($(element).parent().hasClass('ui-spinner'))
+        if ($(element).parent().hasClass('ui-spinner'))
             $(element).parent().removeClass('ui-state-error');
+        if ($(element).prop('tagName') == 'INPUT')
+            $(element).removeClass('ui-state-error');
     }
 });
 jQuery.validator.addMethod("greaterThanZero", function (value) {
     return (parseFloat(value) > 0);
 }, "* Amount must be greater than zero");
+jQuery.validator.addMethod("greaterOrEqualZero", function (value) {
+    return (parseFloat(value) >= 0);
+}, "* Amount must be greater or equal zero");
