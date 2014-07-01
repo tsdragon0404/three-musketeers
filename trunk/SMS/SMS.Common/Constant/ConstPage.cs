3d2d0042685e39241dd7a277fe4c3f3bed856ec4
grayboneonline@gmail.com <grayboneonline@gmail.com@c35c649d-0493-fe16-f86e-a3b88fa47fd3>
2014-07-01 14:43:39 +0000

    Administration: role + user

--- trunk/SMS/SMS.Common/Constant/ConstPage.cs
+++ trunk/SMS/SMS.Common/Constant/ConstPage.cs
@@ -6,6 +6,13 @@
         public const int HomePage = 1;
         public const int Cashier = 2;
         public const int Kitchen = 3;
-        public const int ReportViewer = 4;
+        public const int Account = 4;
+        public const int ReportViewer = 5;
+
+        public const int AdminHome = 20;
+        public const int GlobalLabel = 21;
+        public const int Role = 22;
+        public const int User = 23;
+
     }
 }
