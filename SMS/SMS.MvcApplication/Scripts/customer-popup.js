b7febe910c8dac91018764fcd8ad7ac2dac7b66d
tsdragon0404@gmail.com <tsdragon0404@gmail.com@c35c649d-0493-fe16-f86e-a3b88fa47fd3>
2014-08-29 08:59:50 +0000

    add function build search on each column (use for popup)

--- SMS/SMS.MvcApplication/Scripts/customer-popup.js
+++ SMS/SMS.MvcApplication/Scripts/customer-popup.js
@@ -10,12 +10,14 @@
         dialogClass: "no-close",
         autoOpen: false,
         closeOnEscape: true,
-        width: 900,
-        height: 500,
+        width: 950,
+        height: 600,
         modal: true
     });
 
     $(root.popupId + ' .popup-table-header').table();
+    $(root.popupId).sortingTable([1, 2, 3, 4, 5]);
+    $(root.popupId).searchTable([1, 2, 3, 4, 5]);
     
     $(root.popupId + ' .popupClose').unbind('click');
     $(root.popupId + ' .popupClose').button({
@@ -27,12 +29,22 @@
         return false;
     });
     
-    $(root.popupId + ' #useInputCustomer').unbind('click');
-    $(root.popupId + ' #useInputCustomer').click(function () {
+    $(root.popupId + ' #useInputCustomer').unbind('change');
+    $(root.popupId + ' #useInputCustomer').change(function () {
         if ($(this).is(':checked')) {
+            $(root.popupId + ' #customerInfo table *').prop("disabled", false);
+            $(root.popupId + ' #customerInfo table').removeClass('icon-disable');
 
-        } else {
+            $(root.popupId + ' .popup-table-header *').prop("disabled", true);
+            $(root.popupId + ' .popup-table-header').addClass('icon-disable');
+            $(root.popupId + ' .popup-content *').prop("disabled", true);
+            $(root.popupId + ' .popup-content').addClass('icon-disable');
             
+            $(root.popupId + ' button[id^="select-"]').unbind('click');
+            $(root.popupId + ' button[id^="select-"]').unbind('keypress');
+            $(root.popupId + ' #customer-table tr').unbind('dblclick');
+        } else {
+            bindEvent();
         }
     });
     
@@ -42,43 +54,64 @@
             primary: "ui-icon-check"
         }
     }).click(function () {
-        root.functionCallback(root.orderId, 0, '', '', '', '');
+        var customerName = $(root.popupId + ' #customerName').val();
+        var address = $(root.popupId + ' #address').val();
+        var cellPhone = $(root.popupId + ' #cellPhone').val();
+        var dob = $(root.popupId + ' #DOB').val();
+        root.functionCallback(root.orderId, 0, customerName, address, cellPhone, dob);
+        $(root.popupId).dialog('close');
     });
 
     this.OpenPopup = function () {
         $.ajax({
             type: 'POST',
-            url: root.getDataUrl
+            url: root.getDataUrl + "/GetCustomer"
         }).done(function (result) {
             if (result.Success) {
                 $(root.popupId + ' #customer-table').html('');
                 $(root.popupId + ' #customer-table').html($('#customer-tmpl').tmpl(result));
-                
-                $('button[id^="select-"]').button({
-                    icons: {
-                        primary: "ui-icon-circle-check"
-                    }
-                }).click(function (e) {
-                    root.select(e);
-                    return false;
-                });
 
-                $(root.popupId + ' button[id^="select-"]').unbind('click');
-                $(root.popupId + ' button[id^="select-"]').keypress(function (e) {
-                    if (e.which == 13) {
-                        $(e.target).trigger('click');
-                    }
-                });
+                $("#DOB").datepicker();
 
-                $(root.popupId + ' #customer-table tr').dblclick(function (e) {
-                    $(e.currentTarget).find('button[id^="select-"]').trigger('click');
-                });
+                bindEvent();
 
                 $(root.popupId).dialog("open");
                 SetHeightPopupContent(root.popupId);
             }
         });
     };
+
+    function bindEvent() {
+        $(root.popupId + ' #customerInfo table *').prop("disabled", true);
+        $(root.popupId + ' #customerInfo table').addClass('icon-disable');
+
+        $(root.popupId + ' .popup-table-header *').prop("disabled", false);
+        $(root.popupId + ' .popup-table-header').removeClass('icon-disable');
+        $(root.popupId + ' .popup-conten *').prop("disabled", false);
+        $(root.popupId + ' .popup-content').removeClass('icon-disable');
+
+        $(root.popupId + ' button[id^="select-"]').unbind('click');
+        $(root.popupId + ' button[id^="select-"]').button({
+            icons: {
+                primary: "ui-icon-circle-check"
+            }
+        }).click(function (e) {
+            root.select(e);
+            return false;
+        });
+
+        $(root.popupId + ' button[id^="select-"]').unbind('keypress');
+        $(root.popupId + ' button[id^="select-"]').keypress(function (e) {
+            if (e.which == 13) {
+                $(e.target).trigger('click');
+            }
+        });
+
+        $(root.popupId + ' #customer-table tr').unbind('dblclick');
+        $(root.popupId + ' #customer-table tr').dblclick(function (e) {
+            $(e.currentTarget).find('button[id^="select-"]').trigger('click');
+        });
+    }
     
     this.select = function (e) {
         $(root.popupId).dialog('close');
