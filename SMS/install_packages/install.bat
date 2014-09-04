e1af025c8439988f6b61e9c2fcaba2ec211d01fa
grayboneonline@gmail.com <grayboneonline@gmail.com@c35c649d-0493-fe16-f86e-a3b88fa47fd3>
2014-09-04 03:48:22 +0000

    change install.bat

--- SMS/install_packages/install.bat
+++ SMS/install_packages/install.bat
@@ -1,16 +1,22 @@
-@echo off
-D:
+@ECHO OFF
+
+SET SCRIPT_PATH=D:\Projects\TM\SMS\install_packages
+SET SQLSERVER=.
+SET USER=sms
+SET PASSWORD=sms
+SET DATABASE=SMS
+
 echo executing Drop schema.sql
-sqlcmd -S EZITWK160 -U sa -P sa123 -d SMSv100 -i "D:\Son Nguyen\Projects\SMS\install_packages\Drop schema.sql"
+sqlcmd -S %SQLSERVER% -U %USER% -P %PASSWORD% -d %DATABASE% -i "%SCRIPT_PATH%\Drop schema.sql"
 
 echo executing Db Schema.sql
-sqlcmd -S EZITWK160 -U sa -P sa123 -d SMSv100 -i "D:\Son Nguyen\Projects\SMS\install_packages\Db Schema.sql"
+sqlcmd -S %SQLSERVER% -U %USER% -P %PASSWORD% -d %DATABASE% -i "%SCRIPT_PATH%\Db Schema.sql"
 
 echo executing data.sql
-sqlcmd -S EZITWK160 -U sa -P sa123 -d SMSv100 -i "D:\Son Nguyen\Projects\SMS\install_packages\data.sql"
+sqlcmd -S %SQLSERVER% -U %USER% -P %PASSWORD% -d %DATABASE% -i "%SCRIPT_PATH%\data.sql"
 
 echo executing data-sample.sql
-sqlcmd -S EZITWK160 -U sa -P sa123 -d SMSv100 -i "D:\Son Nguyen\Projects\SMS\install_packages\data-sample.sql"
+sqlcmd -S %SQLSERVER% -U %USER% -P %PASSWORD% -d %DATABASE% -i "%SCRIPT_PATH%\data-sample.sql"
 
 if errorlevel 1 goto pauseError
 
