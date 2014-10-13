374e149c32f3fb0c5b9176a6a52704375c85d17d
grayboneonline@gmail.com <grayboneonline@gmail.com@c35c649d-0493-fe16-f86e-a3b88fa47fd3>
2014-10-13 04:01:28 +0000

    fix bug role

--- trunk/SMS/SMS.Business.Impl/RoleManagement.cs
+++ trunk/SMS/SMS.Business.Impl/RoleManagement.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using Core.Common.Validation;
+using SMS.Common.Session;
 using SMS.Data;
 using SMS.Data.Dtos;
 using SMS.Data.Entities;
@@ -12,6 +14,8 @@ namespace SMS.Business.Impl
     {
         #region Fields
 
+        public virtual IUserRepository UserRepository { get; set; }
+
         #endregion
 
         public ServiceResult<IList<RoleDto>> GetByUserID(long userID)
@@ -21,6 +25,29 @@ namespace SMS.Business.Impl
 
             var result = Repository.Find(x => x.UsersInRole.Select(y => y.ID).Contains(userID)).ToList();
             return ServiceResult<IList<RoleDto>>.CreateSuccessResult(Mapper.Map<IList<RoleDto>>(result));
+        }
+
+        public override ServiceResult<RoleDto> Save(RoleDto dto)
+        {
+            // keep users in this role
+            if(dto.ID != 0)
+                dto.UsersInRole = Mapper.Map<IList<UserDto>>(Repository.Get(dto.ID).UsersInRole);
+
+            var result = new ServiceResult<RoleDto>();
+
+            var entity = Mapper.Map<Role>(dto);
+            entity.Branch = new Data.Entities.Branch { ID = SmsSystem.SelectedBranchID };
+
+            if (entity.ID == 0)
+                Repository.Add(entity);
+            else
+            {
+                var mergeEntity = Repository.Merge(entity);
+                Repository.Update(mergeEntity);
+                entity = mergeEntity;
+            }
+            result.Data = Mapper.Map<RoleDto>(entity);
+            return result;
         }
     }
 }
