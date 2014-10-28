c2031ca521f83c5f1be752d5143a13140a9b344b
grayboneonline@gmail.com <grayboneonline@gmail.com@c35c649d-0493-fe16-f86e-a3b88fa47fd3>
2014-10-28 15:40:17 +0000

    First api project

---
+++ SMS/SMS.WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace SMS.WebAPI.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "webcome";
+        }
+    }
+}
