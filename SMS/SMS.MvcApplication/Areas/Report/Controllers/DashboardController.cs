5d221dad71a6ef0564e2fab13d91abb6f54181f2
tsdragon0404@gmail.com <tsdragon0404@gmail.com@c35c649d-0493-fe16-f86e-a3b88fa47fd3>
2014-12-19 10:34:52 +0000

    update

--- SMS/SMS.MvcApplication/Areas/Report/Controllers/DashboardController.cs
+++ SMS/SMS.MvcApplication/Areas/Report/Controllers/DashboardController.cs
@@ -1,12 +1,24 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Data;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using Core.Common;
 using SMS.Common.Constant;
 using SMS.Common.CustomAttributes;
 using SMS.MvcApplication.Base;
+using SMS.MvcApplication.Models;
+using SMS.Services;
 
 namespace SMS.MvcApplication.Areas.Report.Controllers
 {
     public class DashboardController : BaseController
     {
+        #region Fields
+
+        public virtual IReportService ReportService { get; set; }
+
+        #endregion
+
         [SmsAuthorize(ConstPage.Report_Dashboard)]
         [PageID(ConstPage.Report_Dashboard)]
         public ActionResult Index()
@@ -14,5 +26,16 @@ namespace SMS.MvcApplication.Areas.Report.Controllers
             return View();
         }
 
+        [HttpPost]
+        public JsonResult GetReportData(string spName, DateTime fromDate, DateTime toDate)
+        {
+            var param = new List<SpParameter>
+                             {
+                                 new SpParameter {Name = "I_vFromDate", Value = fromDate},
+                                 new SpParameter {Name = "I_vToDate", Value = toDate}
+                             };
+            DataTable result = ReportService.ExecuteStoredProcedure(spName, param);
+            return Json(JsonModel.Create(result));
+        }
     }
 }
