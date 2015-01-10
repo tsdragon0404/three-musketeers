5bfceb2769cbc808c05dc35e8c990a6a5c26a524
grayboneonline@gmail.com <grayboneonline@gmail.com@c35c649d-0493-fe16-f86e-a3b88fa47fd3>
2015-01-10 16:51:55 +0000

    add support calling sp

--- SMS/SMS.Data.Impl/Inventory/DynamicDA.cs
+++ SMS/SMS.Data.Impl/Inventory/DynamicDA.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using Core.Data.PetaPoco;
 using SMS.Data.Inventory;
@@ -26,6 +28,29 @@ namespace SMS.Data.Impl.Inventory
         {
             var db = new Database(config);
             return db.Query<TModel>(sql, args).ToList();
+        }
+
+        public void ExecuteStoreProcedure(string spName, params object[] args)
+        {
+            var sqlStr = string.Format(";EXEC {0}", spName);
+
+            if (args.Length > 0)
+            {
+                for (var i = 0; i < args.Length; i++)
+                {
+                    var sqlParam = args[i] as SqlParameter;
+                    if (sqlParam == null || sqlParam.Direction != ParameterDirection.Output)
+                        sqlStr += string.Format(" @{0},", i);
+                    else
+                        sqlStr += string.Format(" @{0} OUTPUT,", i);
+                }
+                
+                sqlStr = sqlStr.Remove(sqlStr.Length - 1);
+            }
+
+            var sql = Sql.Builder.Append(sqlStr, args);
+            
+            new Database(config).Execute(sql);
         }
     }
 }
