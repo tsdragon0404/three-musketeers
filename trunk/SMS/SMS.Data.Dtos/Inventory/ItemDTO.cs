594bc1080e359b94637eab9410dd43525f6dba7a
grayboneonline@gmail.com <grayboneonline@gmail.com@c35c649d-0493-fe16-f86e-a3b88fa47fd3>
2015-01-20 18:54:16 +0000

    Item

---
+++ trunk/SMS/SMS.Data.Dtos/Inventory/ItemDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SMS.Data.Dtos.Inventory
+{
+    public class ItemDto : EnableSortableDto
+    {
+        public long ItemID { get; set; }
+        public string ItemCode { get; set; }
+        public string VNName { get; set; }
+        public string ENName { get; set; }
+        public string VNDescription { get; set; }
+        public string ENDescription { get; set; }
+        public long UnitID { get; set; }
+        public long ProductCategoryID { get; set; }
+        public bool IsInventory { get; set; }
+        public decimal MinQuantity { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+        public string CreatedUser { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public string ModifiedUser { get; set; }
+    }
+}
