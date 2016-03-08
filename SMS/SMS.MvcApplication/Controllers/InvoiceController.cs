6ec4adbbd88edc210c84dee4b5e905a375807409
Vu Thanh Lam <grayboneonline@gmail.com>
2016-03-09 01:04:34 +0700

    eee

--- SMS/SMS.MvcApplication/Controllers/InvoiceController.cs
+++ SMS/SMS.MvcApplication/Controllers/InvoiceController.cs
@@ -3,6 +3,7 @@ using SMS.Common.CustomAttributes;
 using SMS.MvcApplication.Base;
 using SMS.Services;
 using System;
+using System.Globalization;
 using System.Web.Mvc;
 
 namespace SMS.MvcApplication.Controllers
@@ -13,7 +14,9 @@ namespace SMS.MvcApplication.Controllers
     {
         public virtual IInvoiceService InvoiceService { get; set; }
 
-        public ActionResult Index(DateTime? fromDate, DateTime? toDate, int? minAmount, int? maxAmount, string customerName)
+        public ActionResult Index([CustomFormatDateTime("dd-MM-yyyy")]DateTime? fromDate,
+                                  [CustomFormatDateTime("dd-MM-yyyy")]DateTime? toDate, 
+                                  int? minAmount, int? maxAmount, string customerName)
         {
             ViewBag.FromDate = fromDate.HasValue ? fromDate.Value.ToString("dd-MM-yyyy") : null;
             ViewBag.ToDate = toDate.HasValue ? toDate.Value.ToString("dd-MM-yyyy") : null;
@@ -23,6 +26,37 @@ namespace SMS.MvcApplication.Controllers
 
             var list = InvoiceService.SearchInvoice(fromDate, toDate, minAmount, maxAmount, customerName);
             return View(list);
+        }
+    }
+
+    public class CustomFormatDateTimeAttribute : CustomModelBinderAttribute
+    {
+        private IModelBinder _binder;
+
+        public CustomFormatDateTimeAttribute(string format)
+        {
+            _binder = new CustomFormatDateTimeModelBinder(format);
+        }
+
+        public override IModelBinder GetBinder() { return _binder; }
+    }
+
+    public class CustomFormatDateTimeModelBinder : IModelBinder
+    {
+        public string CustomFormat { get; private set; }
+
+        public CustomFormatDateTimeModelBinder(string customFormat)
+        {
+            CustomFormat = customFormat;
+        }
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+            if (value == null || string.IsNullOrEmpty(value.AttemptedValue))
+                return null;
+
+            return DateTime.ParseExact(value.AttemptedValue, CustomFormat, CultureInfo.InvariantCulture);
         }
     }
 }
